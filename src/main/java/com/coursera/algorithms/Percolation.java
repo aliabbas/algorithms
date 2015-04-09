6f718bc8a9bc844e4f0e669f50ee04366f5d640f
Ali Abbas <s.aliabbas@gmail.com>
2015-04-09 18:03:44 -0400

    updated pom.xml with algorithm libs and created Percolate

---
+++ src/main/java/com/coursera/algorithms/Percolation.java
@@ -0,0 +1,30 @@
+package com.coursera.algorithms;
+
+public class Percolation {
+
+	public Percolation(int N) {
+		
+	}
+	
+	public void open(int i, int j) {
+		
+	}
+	
+	public boolean isOpen(int i, int j) {
+		return false;
+	}
+	
+	public boolean isFull(int i, int j) {
+		return false;
+	}
+	
+	public boolean percolates() {
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
